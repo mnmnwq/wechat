90d694a7e110b9416a6c8435a510f64700062740
www <mnmnwq@qq.com>
2019-10-11 16:51:35 +0800

    aa

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -41,4 +41,42 @@ class WechatController extends Controller
         }
        return $wechat_access_token;
     }
+
+    /**
+     * post
+     * @param $url
+     * @param $data
+     * @return bool|string
+     */
+    public function curl_post($url,$data)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false);
+
+        curl_setopt($curl,CURLOPT_POST,true);
+
+        curl_setopt($curl,CURLOPT_POSTFIELDS,$data);
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+        return $result;
+    }
+
+    /**
+     * get
+     * @param $url
+     * @return bool|string
+     */
+    public function curl_get($url)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
+    }
 }
