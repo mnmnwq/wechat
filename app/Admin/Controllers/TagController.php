a7aed5f25ac2691ca9af04a26ebadae38d0ea27c
www <mnmnwq@qq.com>
2019-10-11 21:07:19 +0800

    aa

--- app/Admin/Controllers/TagController.php
+++ app/Admin/Controllers/TagController.php
@@ -9,6 +9,7 @@ use Encore\Admin\Grid;
 use Encore\Admin\Show;
 use Encore\Admin\Widgets\Table;
 use App\Tools\Tools;
+use Illuminate\Http\Request;
 
 
 class TagController extends AdminController
@@ -22,20 +23,43 @@ class TagController extends AdminController
     {
         echo 22222;
     }
+
+    public function del_tag(Request $request)
+    {
+        $req = $request->all();
+        $url = 'https://api.weixin.qq.com/cgi-bin/tags/delete?access_token='.$this->tools->get_access_token();
+        $data = [
+            'tag'=>[
+                'id'=>$req['tag_id']
+            ]
+        ];
+        $re = $this->tools->curl_post($url,json_encode($data));
+        $result = json_decode($re,1);
+        if($result['errcode'] == 0){
+            return redirect('admin/wechat/tag');
+        }
+    }
     public function tag_list()
     {
         $url = 'https://api.weixin.qq.com/cgi-bin/tags/get?access_token='.$this->tools->get_access_token();
         $re = $this->tools->curl_get($url);
         $result = json_decode($re,1);
-        dd($result);
-        $headers = ['标签名称', 'Email', 'Name', 'Company'];
-        $rows = [
+        $headers = ['标签ID','标签名称', '操作'];
+        $rows = [];
+        foreach($result['tags'] as $v){
+            $row = [];
+            $row[] = $v['id'];
+            $row[] = $v['name'];
+            $row[] = '<a href="'.env('APP_URL').'">修改</a> | <a href="'.env('APP_URL').'/admin/wechat/del_tag?tag_id='.$v['id'].'">删除</a>';
+            $rows[] = $row;
+        }
+        /*$rows = [
             [1, 'labore21@yahoo.com', 'Ms. Clotilde Gibson', '<a href="http://www.wechat.com/admin/wechat/add_tag">aaaaaaaa</a>'],
             [2, 'omnis.in@hotmail.com', 'Allie Kuhic', 'Murphy, Koepp and Morar'],
             [3, 'quia65@hotmail.com', 'Prof. Drew Heller', 'Kihn LLC'],
             [4, 'xet@yahoo.com', 'William Koss', 'Becker-Raynor'],
             [5, 'ipsa.aut@gmail.com', 'Ms. Antonietta Kozey Jr.'],
-        ];
+        ];*/
 
         $table = new Table($headers, $rows);
 
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,5 +13,6 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('/wechat/tag', 'TagController@tag_list')->name('admin.tag');
     $router->get('/wechat/add_tag', 'TagController@add_tag')->name('admin.add_tag');
+    $router->get('/wechat/del_tag', 'TagController@del_tag')->name('admin.del_tag');
 
 });
