b47482715b10659eaf65b7da213a009a600aafc8
www <mnmnwq@qq.com>
2019-10-14 15:45:05 +0800

    aa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -22,6 +22,10 @@ class EventController extends Controller
     public function event()
     {
         $info = file_get_contents("php://input");
+        file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+        $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
+        $xml_arr = (array)$xml_obj;
+        dd($xml_arr);
     }
 }
