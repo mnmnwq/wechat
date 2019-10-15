b8277d6f488dcd594114745a8599e333cbaf9fca
www <mnmnwq@qq.com>
2019-10-15 15:39:43 +0800

    aa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,6 +26,7 @@ class EventController extends Controller
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
         $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
         $xml_arr = (array)$xml_obj;
-        dd($xml_arr);
+        $msg = '你好！';
+        echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
     }
 }
