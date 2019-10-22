229ffca08a25ee7a13ecf9b1c53c4b06fc148e26
www <mnmnwq@qq.com>
2019-10-22 10:34:33 +0800

    a

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\UserWechat;
 use Illuminate\Http\Request;
 use App\Tools\Tools;
 
@@ -29,8 +30,45 @@ class EventController extends Controller
         //关注操作
         if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
             $wechat_user = $this->tools->get_wechat_user($xml_arr['FromUserName']);
-            $msg = '你好'.$wechat_user['nickname'].'，欢迎关注我的公众号！';
+            $msg = '欢迎'.$wechat_user['nickname'].'同学'.'，感谢您的关注';
             echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+        }
+        //签到
+        if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'CLICK' && $xml_arr['EventKey'] == 'sign'){
+            //签到
+            //判断是否签到
+            $usere_wechat = UserWechat::where(['openid'=>$xml_arr['FromUserName']])->first();
+            $today = date('Y-m-d',time()); //今天
+            $last_day = date('Y-m-d',strtotime("-1 days")); //昨天
+            if($usere_wechat->sign_day == $today){
+                //已经签到
+                $msg = '您已签到';
+                echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+            }else{
+                //根据签到次数加积分
+                //连续签到
+                if($usere_wechat->sign_day == $last_day){
+                    //连续签到
+                    $sign_num = $usere_wechat->sign_num + 1;
+                    if($sign_num >= 6){
+                        $sign_num = 1;
+                    }
+                    UserWechat::where(['openid'=>$xml_arr['FromUserName']])->update([
+                        'sign_day'=>$today,
+                        'sign_num'=>$sign_num,
+                        'sign_score'=>$usere_wechat->sign_score + 5 * $sign_num
+                    ]);
+                }else{
+                    //非连续签到
+                    UserWechat::where(['openid'=>$xml_arr['FromUserName']])->update([
+                        'sign_day'=>$today,
+                        'sign_num'=>1,
+                        'sign_score'=>$usere_wechat->sign_score + 5
+                        ]);
+                }
+
+            }
+
         }
         //普通消息
         if($xml_arr['MsgType'] == 'text' && $xml_arr['Content'] == '1111'){
